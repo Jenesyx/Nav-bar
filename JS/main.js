a3f837c187202a43d576acd4937e74b03b8da8f5
Ali Bidkhori <a.bidkhori2004@gmail.com>
2021-11-01 12:21:59 +0100

    1.0 Update - added all codes

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+function menuToggle(){
+    let nav = document.querySelector('#nav');
+    let toggle = document.querySelector('#toggle');
+    nav.classList.toggle('active')
+    toggle.classList.toggle('active');
+}
